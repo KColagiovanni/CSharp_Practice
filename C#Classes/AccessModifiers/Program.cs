3a520a9ffcdccd9057760ca7824d27f1227dd18e
Kevin Colagiovanni <76464469+KColagiovanni@users.noreply.github.com>
2026-01-23 22:57:28 -0800

    Updated AccessModifiers. Created Properties.

--- C#Classes/AccessModifiers/Program.cs
+++ C#Classes/AccessModifiers/Program.cs
@@ -1,26 +1,33 @@
 ﻿/*
-The public keyword is an access modifier, which is used to set the access level/visibility for classes, fields, methods and properties.
+The public keyword is an access modifier, which is used to set the access level/visibility for classes, fields, 
+methods and properties.
 
 C# has the following access modifiers:
 
 Modifier	Description
-public	The code is accessible for all classes
-private	The code is only accessible within the same class
-protected	The code is accessible within the same class, or in a class that is inherited from that class. You will learn more about inheritance in a later chapter
-internal	The code is only accessible within its own assembly, but not from another assembly. You will learn more about this in a later chapter
-There's also two combinations: protected internal and private protected.
+public:	    - The code is accessible for all classes.
+private:	- The code is only accessible within the same class.
+protected:	- The code is accessible within the same class, or in a class that is inherited from that class.
+internal:	- The code is only accessible within its own assembly, but not from another assembly.
+**There's also two combinations: protected internal and private protected.
 
-If you declare a field with a private access modifier, it can only be accessed within the same class:
+If you declare a field with a private access modifier, it can only be accessed within the same class.
+
+Why Access Modifiers?
+To control the visibility of class members (the security level of each individual class and class member).
+To achieve "Encapsulation" - which is the process of making sure that "sensitive" data is hidden from users. This is 
+done by declaring fields as private.
+**Note: By default, all members of a class are private if you don't specify an access modifier.
 */
 
 class Program
 {
   static void Main(string[] args)
   {
-    CarPrivate myObj = new CarPrivate();
-    Console.WriteLine(myObj.model);
- 
     CarPublic myObj = new CarPublic();
     Console.WriteLine(myObj.model);
+
+    // CarPrivate myObj = new CarPrivate();  // Throws and error.
+    // Console.WriteLine(myObj.model); 
   }
 }
